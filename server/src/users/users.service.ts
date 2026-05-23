71777ec7ed58a7f0de78c736db4f10e24f8408a0
Narvin Sachaniya <githubnarvin@gmail.com>
2026-05-23 18:28:44 +0530

    Add users service

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  async create(email: string, password: string) {
+    const hashed = await bcrypt.hash(password, 10);
+    const user = this.repo.create({ email, password: hashed });
+    return this.repo.save(user);
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+
+  findById(id: string) {
+    return this.repo.findOne({ where: { id } });
+  }
+}
