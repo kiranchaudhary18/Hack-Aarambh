2cff2b22cba0f8fb52e3049a0fbe9004fac339f1
Narvin Sachaniya <githubnarvin@gmail.com>
2026-05-29 22:32:47 +0530

    feat(users): add user module, service and controller

--- server/src/users/user.entity.ts
+++ server/src/users/user.entity.ts
@@ -1,8 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column({ unique: true })
@@ -11,9 +17,27 @@ export class User {
   @Column()
   password: string;
 
-  @Column({ default: 'user' })
+  @Column({ default: "user" })
   role: string;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ nullable: true })
+  avatar: string;
+
+  @Column({ default: 0 })
+  scansUsed: number;
+
+  @Column({ default: 20 })
+  scansLimit: number;
+
+  @Column({ default: "free" })
+  plan: string;
 
   @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-import * as bcrypt from 'bcrypt';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
@@ -20,5 +20,31 @@ export class UsersService {
 
   findById(id: string) {
     return this.repo.findOne({ where: { id } });
+  }
+
+  async updateProfile(id: string, data: { name?: string; avatar?: string }) {
+    const user = await this.findById(id);
+    if (!user) return null;
+
+    if (data.name !== undefined) user.name = data.name;
+    if (data.avatar !== undefined) user.avatar = data.avatar;
+
+    return this.repo.save(user);
+  }
+
+  async incrementScans(id: string) {
+    const user = await this.findById(id);
+    if (!user) return null;
+
+    user.scansUsed = (user.scansUsed || 0) + 1;
+    return this.repo.save(user);
+  }
+
+  async getProfile(id: string) {
+    const user = await this.findById(id);
+    if (!user) return null;
+
+    const { password, ...profile } = user;
+    return profile;
   }
 }
