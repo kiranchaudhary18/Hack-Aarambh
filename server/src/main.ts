0e8a40cc7178d9d371b764e2169720f923457345
Narvin Sachaniya <githubnarvin@gmail.com>
2026-05-31 15:53:28 +0530

    Update server main entry point

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { jwtMiddleware } from "./common/jwt.middleware";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(jwtMiddleware);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: false }));
   app.enableCors();
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
   console.log("Server running on port", process.env.PORT || 3000);
