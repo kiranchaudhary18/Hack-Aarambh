e303192589aa4a2f768fa234a8df1af624d9a107
Narvin Sachaniya <githubnarvin@gmail.com>
2026-05-31 15:53:28 +0530

    Update authentication module configuration

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,14 +1,19 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersModule } from "../users/users.module";
+import { EmailModule } from "../email/email.module";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { PasswordReset } from "./password-reset.entity";
 
 @Module({
   imports: [
     UsersModule,
+    EmailModule,
+    TypeOrmModule.forFeature([PasswordReset]),
     JwtModule.register({
-      secret: process.env.JWT_SECRET || "change_this_secret",
+      secret: "ca5d924a04711ae040e1f9118f07908a7a1bcaa02c4337272d52afd7a7b9ea14",
       signOptions: { expiresIn: "7d" },
     }),
   ],
